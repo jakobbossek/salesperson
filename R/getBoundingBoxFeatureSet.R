c49c1c33bbaa2cd117462c835b6e0a4ea0821cef
Jakob Bossek <info@jakobbossek.de>
2015-02-06 21:38:36 +0100

    fix warnings of R CMD check

--- R/getBoundingBoxFeatureSet.R
+++ R/getBoundingBoxFeatureSet.R
@@ -1,14 +1,15 @@
 #' Feature: fraction of points near bounding boxplot.
 #'
-#' @param x [\code{\link[netgen]{Network}}]\cr
+#FIXME: netgen needs a documentation for 'Network' class objects
+#' @param x [\code{Network}]\cr
 #'   Network.
 #' @param distance_fraction [\code{numeric}]\cr
 #'   Fraction of distance between min and max coordinates of each dimension
 #'   which should points be allowed lie inbetween.
 #' @return [\code{list}]
 #' @export
-getFractionOfPointsNearBoundingBox = function(x, distance_fraction = 0.1) {
+getBoundingBoxFeatureSet = function(x, distance_fraction = 0.1) {
     assertClass(x, "Network")
-    assertNumber(distance_fraction, min = 0.00001, upper = 0.4999, na.ok = FALSE)
+    assertNumber(distance_fraction, lower = 0.00001, upper = 0.4999, na.ok = FALSE)
     return(getFractionOfPointsNearBoundingBoxCPP(x$coordinates, distance_fraction))
 }
