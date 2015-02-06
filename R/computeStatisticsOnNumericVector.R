9ccd3295cfcbad9a527ccb7c5df418854a5eab61
Jakob Bossek <info@jakobbossek.de>
2015-02-06 21:54:00 +0100

    add helper for all the statistic measures on numeric vectors

---
+++ R/computeStatisticsOnNumericVector.R
@@ -0,0 +1,26 @@
+# Helper to compute default statistics of numeric vector.
+#
+# @param x [Network]
+#   Network.
+# @param type [character(1)]
+#   Type of statistics.
+# @return [list]
+computeStatisticsOnNumericVector = function(x, type) {
+    x.mean = mean(x)
+    x.sd = sd(x)
+    x.min = min(x)
+    x.max = max(x)
+    feats = list(
+        "mean" = x.mean,
+        "sd" = x.sd,
+        "var" = var(x),
+        "median" = median(x),
+        "varcoeff" = x.sd / x.mean,
+        "min" = x.min,
+        "max" = x.max,
+        #FIXME: span in x and y dimension?
+        "span" = x.max - x.min
+    )
+    names(feats) = paste(type, names(feats), sep = "_")
+    return(feats)
+}
