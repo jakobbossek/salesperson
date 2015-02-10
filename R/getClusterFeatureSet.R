388221a253d37d370d0e39676410f236922d2e57
Jakob Bossek <info@jakobbossek.de>
2015-02-10 19:03:52 +0100

    add getClusterFeatureSet

---
+++ R/getClusterFeatureSet.R
@@ -0,0 +1,14 @@
+#' Feature: statistics of the minimum spanning tree edges.
+#'
+#' @template arg_network
+#' @param epsilon [\numeric(1)]\cr
+#'   Probability for reachability computation in dbscan clustering method.
+#'   See \code{\link[tspmeta]{feature_cluster}}. Default is 0.01.
+#' @return [\code{list}]
+#' @export
+getClusterFeatureSet = function(x, epsilon = 0.01) {
+    assertNumber(epsilon, lower = 0.001, upper = 1, na.ok = FALSE)
+    # here we delegate to tspmeta
+    tsp.instance = tspmeta::tsp_instance(x$coordinates)
+    tspmeta::feature_cluster(tsp.instance, epsilon = epsilon)
+}
