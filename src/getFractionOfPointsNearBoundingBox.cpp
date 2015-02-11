33a1dfc411460413da2d421dcc7d5a9cafee8e26
Jakob Bossek <info@jakobbossek.de>
2015-02-11 09:12:27 +0100

    cleanup

--- src/getFractionOfPointsNearBoundingBox.cpp
+++ src/getFractionOfPointsNearBoundingBox.cpp
@@ -50,24 +50,3 @@ List getFractionOfPointsNearBoundingBoxCPP(NumericMatrix coords, double distance
         _[feature_name] = NumericVector::create(n_out_of_bounds / n_cities)
     );
 }
-
-
-// feature_bounding_box = function(x, distance_fraction = 0.1) {
-//     coords = x$coords
-//     ranges = apply(coords, 2, range)
-//     distances = diff(ranges)
-//     distance_x = distances[1] * distance_fraction
-//     distance_y = distances[2] * distance_fraction
-//     x_min = ranges[1,1] + distance_x
-//     x_max = ranges[2,1] - distance_x
-//     y_min = ranges[1,2] + distance_y
-//     y_max = ranges[2,2] - distance_y
-//     idx = which(coords[, 1] < x_min | coords[, 1] > x_max | coords[, 2] < y_min | coords[, 2] > y_max)
-//     if (length(idx) == 0) {
-//         idx = 0
-//     }
-//     res = list(ratio_of_cities_outside_box = length(unique(idx)) / number_of_cities(x))
-//     prefix = sprintf("bounding_box_%02i", floor(distance_fraction * 100))
-//     names(res) = paste(prefix, names(res), sep = "_")
-//     res
-// }
--- src/getNearestNeighbourDistances.cpp
+++ src/getNearestNeighbourDistances.cpp
@@ -6,8 +6,6 @@ NumericVector getNearestNeighbourDistancesCPP(NumericMatrix dist_mat) {
     int n_nodes = dist_mat.nrow();
     NumericVector nn_dists(n_nodes);
 
-    // FIXME: we can make this faster! Distances are symmetric and as a consequence
-    // if j is the nearest neighbour of i, than i is nearest neighbour of j.
     for (int i = 0; i < n_nodes; ++i) {
         double nn_dist = INFINITY;
         for (int j = 0; j < n_nodes; ++j) {
@@ -20,6 +18,5 @@ NumericVector getNearestNeighbourDistancesCPP(NumericMatrix dist_mat) {
         }
         nn_dists[i] = nn_dist;
     }
-
     return nn_dists;
 }
