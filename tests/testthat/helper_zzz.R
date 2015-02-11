7e4b9c2b3dcf64ef2346db20b15c20c89c00bf14
Jakob Bossek <info@jakobbossek.de>
2015-02-11 16:14:53 +0100

    add helper_zzz with some helpers for the test cases

---
+++ tests/testthat/helper_zzz.R
@@ -0,0 +1,40 @@
+set.seed(1)
+
+# define some helpers
+
+# Generate simple network with three nodes.
+#
+# Test network geography:
+# (0,1)   (1,1)
+# o       o
+#
+#
+#
+# o
+# (0,0)
+# @return [Network]
+generateSimpleTestNetwork = function() {
+    x = list()
+    x$coordinates = matrix(c(0, 0, 1, 1, 0, 1), ncol = 2, byrow = TRUE)
+    x$distance.matrix = dist(x$coordinates)
+    class(x) = "Network"
+    return(x)
+}
+
+
+# Check that object is feature list
+# @param x [any]
+#   Object to check.
+# @param feature.set [character(1)]
+#   Name of the feature set (for debugging).
+expect_feature_list = function(x, feature.set) {
+    expect_true(is.list(x))
+    expect_true(!is.null(attr(x, "time.elapsed")))
+    expect_true(is.numeric(attr(x, "time.elapsed")))
+    for (i in 1:length(x)) {
+        expect_true(is.numeric(x[[i]]),
+            info = sprintf("Feature '%s' of feature set '%s' is NOT numeric.",
+            names(x)[i], feature.set)
+        )
+    }
+}
