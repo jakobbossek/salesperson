2bb53feca73618fcc1981998487bf566630769a7
Jakob Bossek <info@jakobbossek.de>
2015-08-20 11:15:05 +0200

    cleanup

--- R/helpers.R
+++ R/helpers.R
@@ -12,6 +12,16 @@ removeFileExtension = function(filename) {
   collapse(x[-n], sep = ".")
 }
 
+#' Convert netgen object to tspmeta TSP instance.
+#'
+#' @param x [Network]
+#'   Network.
+#' @return [tsp_instance]
+netgenToTSPmeta = function(x) {
+  structure(list(coords = x$coordinates, dists = x$distance.matrix),
+    class = c("tsp_instance_euclidean_coords", "tsp_instance_symmetric", "tsp_instance"))
+}
+
 #' Compute tour length given a TSP instance and a tour/permutation.
 #'
 #' @param x [\code{Network}]\cr
