775b688c92e59f4eb32b7e28672fa3b916bf82a8
Jakob Bossek <info@jakobbossek.de>
2017-10-04 17:04:24 -0700

    update DESCRIPTION: update data, URLs and import new packages

--- R/zzz.R
+++ R/zzz.R
@@ -6,11 +6,14 @@
 #' @import ParamHelpers
 #' @import TSP
 #' @import ecr
+#' @import cccd
+#' @import igraph
 #' @importFrom fpc dbscan
 #' @importFrom splancs areapl
 #' @importFrom vegan spantree spandepth
 #' @importFrom grDevices chull
-#' @importFrom stats median quantile sd var
+#' @importFrom stats median quantile sd var stats density dist rnorm runif
 #' @importFrom utils getS3method
 #' @useDynLib salesperson
+#' importFrom()
 NULL
