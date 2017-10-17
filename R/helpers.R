6857cfcb78e08ddfb73bf31936ce0c305d0e63ee
Jakob Bossek <info@jakobbossek.de>
2017-10-17 11:50:49 -0700

    cleanup

--- R/helpers.R
+++ R/helpers.R
@@ -1,8 +1,8 @@
-#' Removes the file extension of a given filename.
-#'
-#' @param filename [character(1)]
-#'   Filename.
-#' @return Truncated filename, i.e., filename without last file extension.
+# Removes the file extension of a given filename.
+#
+# @param filename [character(1)]
+#   Filename.
+# @return Truncated filename, i.e., filename without last file extension.
 removeFileExtension = function(filename) {
   # split by path
   x = str_split(filename, pattern = "\\.")
@@ -12,67 +12,23 @@ removeFileExtension = function(filename) {
   collapse(x[-n], sep = ".")
 }
 
-#' Convert netgen object to tspmeta TSP instance.
-#'
-#' @param x [Network]
-#'   Network.
-#' @return [tsp_instance]
+# Convert netgen object to tspmeta TSP instance.
+#
+# @param x [Network]
+#   Network.
+# @return [tsp_instance]
 netgenToTSPmeta = function(x) {
   structure(list(coords = x$coordinates, dists = x$distance.matrix),
     class = c("tsp_instance_euclidean_coords", "tsp_instance_symmetric", "tsp_instance"))
 }
 
-#' Extracts tour from TSPlib file.
-#'
-#' Since we need only the tour and the tour length, this functions skips the
-#' specification part.
-#'
-#' @param file.path [\code{character(1)}]\cr
-#'   Path to TSPlib tour file.
-#' @return [\code{integer}]
-#'   Permutation of the nodes.
-#' @export
-readTSPlibTOURFile = function(file.path) {
-  assertFile(file.path, access = "r")
-  con = file(file.path, "r")
-
-  # close connection on exit
-  on.exit(close(con))
-
-  tour = numeric()
-  tour.length = NA
-
-  lines = readLines(con)
-  obj = list()
-  i = 1L
-  while (stringr::str_detect(lines[i], ":")) {
-    spec = unlist(strsplit(lines[i], "[[:space:]]*:[[:space:]]*"))
-    if (stringr::str_detect(spec[2], "Length")) {
-      # If there is a line COMMENT: Length = <tour-length>
-      # extract the <tour-length>
-      tour.length = as.numeric(unlist(strsplit(spec[2], "[[:space:]]*=[[:space:]]*"))[2])
-    }
-    obj[[spec[1]]] = spec[2]
-    i = i + 1L
-  }
-  # skip TOUR_SECTION line
-  i = i + 1L
-
-  #INFO: LKH tours contain the negative of the first node ID as the last element
-  while (lines[i] != "EOF" && as.numeric(lines[i]) > 0) {
-    tour = c(tour, as.integer(lines[i]))
-    i = i + 1L
-  }
-  return(list(tour = as.integer(tour), tour.length = tour.length))
-}
-
-#' Check if vector contains permutation of number 1:n
-#'
-#' @param x [\code{numeric}]\cr
-#'   Vector to check.
-#' @param source [\code{numeric}]\cr
-#'   Vector which we want to compare with. Default is 1, ..., length(x).
-#' @return [\code{logical(1)}]
+# Check if vector contains permutation of number 1:n
+#
+# @param x [\code{numeric}]\cr
+#   Vector to check.
+# @param source [\code{numeric}]\cr
+#   Vector which we want to compare with. Default is 1, ..., length(x).
+# @return [\code{logical(1)}]
 isPermutation = function(x, source = seq(length(x))) {
   return(all(source == sort(x)))
 }
