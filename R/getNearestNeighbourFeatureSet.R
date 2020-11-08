efe6938c4f9f551441892f61c68b254005e55764
Jonathan Heins <jonathan.heins@gmail.com>
2020-11-08 01:40:57 +0100

    add normalization for the nearest neighbour features

--- R/getNearestNeighbourFeatureSet.R
+++ R/getNearestNeighbourFeatureSet.R
@@ -9,6 +9,15 @@ getNearestNeighbourFeatureSet = function(x, include.costs = FALSE, ...) {
   assertClass(x, "Network")
   measureTime(expression({
     nn.dists = getNearestNeighbourDistancesCPP(x$distance.matrix)
-    computeStatisticsOnNumericVector(nn.dists, "nearest_neighbour")
+    statistics.on.nn.dists = computeStatisticsOnNumericVector(nn.dists, "nearest_neighbour")
+    d.max = getDMax(x$coordinates)
+    dist.max = computeL2Norm(c(getWidth(x$coordinates), getHeight(x$coordinates)))
+    c(statistics.on.nn.dists,
+      "nearest_neighbour_norm_mean" = normalizeFeature(statistics.on.nn.dists$nearest_neighbour_mean, d.max),
+      "nearest_neighbour_norm_median" = normalizeFeature(statistics.on.nn.dists$nearest_neighbour_median, d.max),
+      "nearest_neighbour_norm_min" = normalizeFeature(statistics.on.nn.dists$nearest_neighbour_min, d.max),
+      "nearest_neighbour_norm_max" = normalizeFeature(statistics.on.nn.dists$nearest_neighbour_max, dist.max),
+      "nearest_neighbour_norm_span" = normalizeFeature(statistics.on.nn.dists$nearest_neighbour_span, dist.max)
+      )
   }), "nearest_neighbor", include.costs)
 }
